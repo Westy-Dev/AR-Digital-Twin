27f7f562f349ac98c312a15bd9e11c91078b8959
BenWestcott <ben.westcott@gmail.com>
2020-07-30 16:30:48 +0100

    Turned highlight off for now as discussion needs to occur

--- Assets/Scripts/InstructionPieceMovement.cs
+++ Assets/Scripts/InstructionPieceMovement.cs
@@ -18,7 +18,7 @@ public class InstructionPieceMovement: MonoBehaviour
     void Start()
     {
         SetMovementPositions(direction);
-        if (highlight)
+        if (false)
         {
             SetHighlight();
         }
