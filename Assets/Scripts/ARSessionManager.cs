a12b223db26466bddd4096dc8f0f17da37283014
BenWestcott <ben.westcott@gmail.com>
2020-08-27 12:50:44 +0100

    Commented code, added Unity Tooltips and refactored code. Updated UI placements now that the debug button has been made invisible for testing.

--- Assets/Scripts/ARSessionManager.cs
+++ Assets/Scripts/ARSessionManager.cs
@@ -1,25 +1,31 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-
-
+/// <summary>
+/// Manages the position, scale, rotation and display of the <c>CurrentModel</c>
+/// </summary>
 public class ARSessionManager : MonoBehaviour
 {
     //Provide in order of build
-    [Tooltip("Provide Folder Name With Model Instructions")]
-    public string InstructionFolderName;
-    private GameObject[] modelInstructions;
-    //The position to instantiate the models in the world
+    [Tooltip("Provide Folder Name With Model Instruction Prefabs (Prefabs in folder should be in build order)")]
+    [SerializeField]
+    private string InstructionFolderName;
 
-    public Transform InstructionsStartPosition;
+    [Tooltip("Provide A Start Position For The Model")]
+    [SerializeField]
+    private Transform InstructionsStartPosition;
+
+    //Holds all prefabs for the model instructions
+    private GameObject[] modelInstructions;
+
+    //Initial values used for reset functionality
     private Vector3 assemblyInitialPosition;
     private Vector3 assemblyInitialScale;
     private Quaternion assemblyInitialRotation;
 
     private int currentInstructionIndex;
     private int lastInstructionIndex;
+
+    //The current displayed prefab
     private GameObject currentModel;
 
     [SerializeField]
@@ -27,23 +33,28 @@ public class ARSessionManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        currentInstructionIndex = 0;
-
+        //Hide previous button on UI
         uiManager.prevButton.SetActive(false);
-        // Instantiates a Prefab located in any Resources
-        // folder in your project's Assets folder with the name provided in the inspector
+
+        //Loads all model instruction prefabs from resources folder using the given instruction folder name in the inspector
         modelInstructions = Resources.LoadAll<GameObject>(InstructionFolderName);
 
+        //As the resources load all function does not load in order, we need to sort the prefabs by name to get them in order
         Array.Sort(modelInstructions, delegate (GameObject x, GameObject y) { return int.Parse(x.name).CompareTo(int.Parse(y.name)); });
 
         if (modelInstructions !=null || modelInstructions.Length > 0)
         {
+            //Sets the last instruction index
             lastInstructionIndex = modelInstructions.Length - 1;
 
+            //Instantiates the first instruction at the start position set in the inspector
             currentModel = Instantiate(modelInstructions[currentInstructionIndex],InstructionsStartPosition);
+
+            //Set the local position relative to the start position to zero 
+            //(this makes sure the model is always located exactly at the instruction start position)
             currentModel.transform.localPosition = Vector3.zero;
 
+            //Set the initial values used for reset functionality
             assemblyInitialPosition = currentModel.transform.localPosition;
             assemblyInitialScale = currentModel.transform.localScale;
             assemblyInitialRotation = currentModel.transform.rotation;
@@ -57,6 +68,9 @@ public class ARSessionManager : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Loads the next instruction from <c>modelInstructions</c>
+    /// </summary>
     public void loadNextInstruction()
     {
         if (currentInstructionIndex != lastInstructionIndex)
@@ -68,6 +82,9 @@ public class ARSessionManager : MonoBehaviour
         updateUIButtons();
     }
 
+    /// <summary>
+    /// Loads the previous instruction from <c>modelInstructions</c>
+    /// </summary>
     public void loadPreviousInstruction()
     {
         if (currentInstructionIndex != 0)
@@ -79,6 +96,9 @@ public class ARSessionManager : MonoBehaviour
         updateUIButtons();
     }
 
+    /// <summary>
+    /// Controls the visibility of the previous and next buttons on the UI
+    /// </summary>
     private void updateUIButtons()
     {
 
@@ -101,7 +121,9 @@ public class ARSessionManager : MonoBehaviour
         }
 
     }
-    
+    /// <summary>
+    /// Updates the number of parts per step and step number on the UI
+    /// </summary>
     private void updateUIText()
     {
         int numberOfMovingPartsForInstruction = getNumberOfMovingPartsForInstruction(currentModel);
@@ -109,18 +131,32 @@ public class ARSessionManager : MonoBehaviour
         uiManager.UpdateStepNumber(currentInstructionIndex + 1);
     }
 
-    public void resetPosition()
+    /// <summary>
+    /// Resets the position of the current instruction model displayed to the original scale, position and rotation.
+    /// </summary>
+    public void resetModel()
     {
         currentModel.transform.localPosition = assemblyInitialPosition;
         currentModel.transform.localScale = assemblyInitialScale;
         currentModel.transform.rotation = assemblyInitialRotation;
     }
 
+    /// <summary>
+    /// Loads the new model instruction from <c>modelInstructions</c> given the instruction index to load
+    /// </summary>
+    /// <param name="currentInstructionIndex">The instruction index number of the instruction to load</param>
     private void loadNewModel(int currentInstructionIndex)
     {
+        //Keep a reference to the current transform of the model
         Transform currentTransform = currentModel.transform;
+
+        //Remove the current model
         Destroy(currentModel);
+
+        //Instantiate the new model instruction at the given instruction start position
         currentModel = Instantiate(modelInstructions[currentInstructionIndex], InstructionsStartPosition);
+
+        //Offset the local position relative to the instruction start position using the original transform reference
         currentModel.transform.localPosition = currentTransform.localPosition;
         currentModel.transform.localScale = currentTransform.localScale;
         currentModel.transform.rotation = currentTransform.rotation;
@@ -128,19 +164,29 @@ public class ARSessionManager : MonoBehaviour
         updateUIText();
     }
 
+    /// <summary>
+    /// Recursively obtains the number of moving parts for the current instruction by checking all game objects 
+    /// (and child objects) in the current instruction for the <c>InstructionPieceMovement</c> script
+    /// </summary>
+    /// <param name="currentModel"></param>
+    /// <returns>The number of moving parts for the current instruction model</returns>
     private int getNumberOfMovingPartsForInstruction(GameObject currentModel)
     {
         int numberOfMovingPartsForInstruction = 0;
+
+        // checks every part in the current model to see if it has a movement script attached
         foreach(Transform part in currentModel.transform)
         {
             InstructionPieceMovement movementScript = part.GetComponent<InstructionPieceMovement>();
             if (movementScript != null)
             {
+                //If the part has a movement script, it is moving and therefore a step in the current instruction
                 numberOfMovingPartsForInstruction++;
             }
             
             if(part.childCount > 0)
             {
+                //Recursively check the child objects
                 numberOfMovingPartsForInstruction += getNumberOfMovingPartsForInstruction(part.gameObject);
             }
         }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,53 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-
+/// <summary>
+/// Manages the UI interaction elements. Used for button events.
+/// </summary>
 public class UIManager : MonoBehaviour
 {
+    [Tooltip("ARSessionManager to control AR Model Instructions")]
     [SerializeField]
     private ARSessionManager arSessionManager;
 
+    [Tooltip("Debug Canvas to help with debugging during runtime")]
     [SerializeField]
     private GameObject debugCanvas;
 
+    [Tooltip("Instruction background panel to obscure camera output")]
     [SerializeField]
     private GameObject instructionBackground;
 
+    [Tooltip("A text object for number of parts in current instruction display")]
     [SerializeField]
     private Text NumberOfPartsForInstruction;
+
+    [Tooltip("A text object for step number display")]
     [SerializeField]
     private Text StepNumber;
 
+    [Tooltip("Button to toggle the background instruction on and off")]
     [SerializeField]
     private Button backgroundButton;
+
+    [Tooltip("Button to toggle the debug canvas on and off")]
     [SerializeField]
     private Button debugButton;
 
+    [Tooltip("Button to move to the previous instruction")]
     public GameObject prevButton;
 
+    [Tooltip("Button to move to the next instruction")]
     public GameObject nextButton;
 
     private bool debug = false;
     private bool showInstructionBackground = false;
+
     private Color32 toggleColor = new Color32(159, 159, 159, 255);
     private Color defaultColor = Color.white;
+
+    /// <summary>
+    /// Loads the next instruction using <c>ARSessionManager</c>
+    /// </summary>
     public void loadNextInstruction()
     {
         arSessionManager.loadNextInstruction();
     }
 
+    /// <summary>
+    /// Loads the previous instruction using <c>ARSessionManager</c>
+    /// </summary>
     public void loadPreviousInstruction()
     {
         arSessionManager.loadPreviousInstruction();
     }
 
+    /// <summary>
+    /// Toggles the display of the <c>DebugCanvas</c>
+    /// </summary>
     public void toggleDebug()
     {
         debug = !debug;
 
         debugCanvas.SetActive(debug);
 
+        //Sets the colour of the debug button to look like a toggle
         if (debug)
         {
             debugButton.image.color = toggleColor;
@@ -58,13 +81,16 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Toggles the display of the <c>InstructionBackground</c>
+    /// </summary>
     public void toggleInstructionBackground()
     {
         showInstructionBackground = !showInstructionBackground;
 
         instructionBackground.SetActive(showInstructionBackground);
 
-
+        //Sets the colour of the background button to look like a toggle
         if (showInstructionBackground)
         {
             backgroundButton.image.color = toggleColor;
@@ -73,17 +99,27 @@ public class UIManager : MonoBehaviour
             backgroundButton.image.color = defaultColor;
         }   
     }
-
-    public void resetPosition()
+    /// <summary>
+    /// Resets the position of the instructions using <c>ARSessionManager</c>
+    /// </summary>
+    public void resetModel()
     {
-        arSessionManager.resetPosition();
+        arSessionManager.resetModel();
     }
 
+    /// <summary>
+    ///  Updates the <c>NumberOfPartsForInstruction</c> text element with the given number of parts
+    /// </summary>
+    /// <param name="numberOfParts"> The number of parts </param>
     public void UpdateNumberOfMovingPartsForInstruction(int numberOfParts)
     {
         NumberOfPartsForInstruction.text = "Parts in Step: " + numberOfParts;
     }
 
+    /// <summary>
+    ///  Updates the <c>StepNumber</c> text element with the given step number
+    /// </summary>
+    /// <param name="stepNumber"> The step number </param>
     public void UpdateStepNumber(int stepNumber)
     {
         StepNumber.text = "Step Number: " + stepNumber;
